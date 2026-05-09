7c2f156ad0707f2478c0d61bf451912175fde996
mritunjay90 <mrityunjayshah981427@gmail.com>
2026-05-09 22:13:32 +0545

    learning

--- Day1/eventloop.mjs
+++ Day1/eventloop.mjs
@@ -1,14 +1,14 @@
-// // call stack
-// function call() {
-//   console.log('call');
-// }
+// call stack
+function call() {
+  console.log('call');
+}
 
-// function call2() {
-//   console.log('call2');
-//   call();
-// }
+function call2() {
+  console.log('call2');
+  call();
+}
 
-// call2();
+call2();
 // console.log('end');
 
 // event loop
@@ -25,3 +25,30 @@ while (i < 1000000000) {
   console.log('loop finished');
 }
 console.log('loop finished');
+    // output
+
+// end
+// loop finished
+// loop finished
+// timeout
+
+// Code for learning loops
+
+// For loop example
+for (let j = 0; j < 5; j++) {
+  console.log(`For loop iteration: ${j}`);
+}
+
+// While loop example
+let k = 0;
+while (k < 5) {
+  console.log(`While loop iteration: ${k}`);
+  k++;
+}
+
+// Do-while loop example
+let m = 0;
+do {
+  console.log(`Do-while loop iteration: ${m}`);
+  m++;
+} while (m < 5);  
